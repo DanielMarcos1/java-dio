241c96c79974dde9285c2b1a1a838a9a9a16b9b0
DanielMarcos1 <daniel04pessoal@gmail.com>
2023-08-25 14:20:57 -0300

    Moving files to fix vscode issue

---
+++ meu-primeiro-projeto-java/src/Classe.java
@@ -0,0 +1,15 @@
+
+public class Classe {
+// Classes são nomeadas com letra maiúscula ex: MinhaClasse
+// Variáveis com camelCase ex: minhaVariavel
+public static void main(String [] args) {
+    String primeiroNome = "Daniel";
+    String segundoNome = "Marcos";
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    System.out.print(nomeCompleto);
+
+}
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return primeiroNome.concat(" ").concat(segundoNome);
+}
+}
