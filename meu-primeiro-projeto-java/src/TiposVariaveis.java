c774b7eb7e9835e8c014623f457f2b40e21c6c51
DanielMarcos1 <daniel04pessoal@gmail.com>
2023-08-25 14:53:58 -0300

    Print variables to the console

--- meu-primeiro-projeto-java/src/TiposVariaveis.java
+++ meu-primeiro-projeto-java/src/TiposVariaveis.java
@@ -1,12 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         // Integers
-        byte javaIntermediario = 1; // 1 byte
-        short javaAvancado = 2; // 2 bytes 
+        byte javaByte = 1; // 1 byte
+        short javaShort = 2; // 2 bytes 
         int javaInt = 4; // 4 byte
-        long javaPro = 8L; // 8 bytes
+        long javaLong = 8L; // 8 bytes
         // Decimals
-        float javaFloat = 4.00F; // 4 bytes
+        float javaFloat = 4.15F; // 4 bytes
         double javaDouble = 8.00; // 8 bytes
 
         // Strong type breaking the code 
@@ -16,6 +16,15 @@ public class TiposVariaveis {
         short numeroNormal2 = (short) numeroNormal; // Here I cast the type, telling the compiler my int value is also a short.
     
         final String JAVA_CONSTANT = "final"; // Constantes em Java são escritos em letra maiúscula e tipo "final".
-        
+
+        System.out.println(javaByte);
+        System.out.println(javaShort);
+        System.out.println(javaInt);
+        System.out.println(javaLong);
+        System.out.println(javaFloat);
+        System.out.println(javaDouble);
+        System.out.println(numeroNormal2);
+        System.out.println(JAVA_CONSTANT);
+
     }
 }
