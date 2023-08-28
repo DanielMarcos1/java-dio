ddcb4a467c132a9517177c3a0f756d8f2d39c6a5
DanielMarcos1 <daniel04pessoal@gmail.com>
2023-08-28 16:23:48 -0300

    Aula de execução pelo terminal

--- estudos-java/src/App.java
+++ estudos-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá, turma!!");
+        System.out.println("Fui executado pelo terminal.");
     }
 }
